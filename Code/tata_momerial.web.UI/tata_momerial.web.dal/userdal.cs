859c906692e60888f0fe9b507ffed0d8945da9d4
Satya P Chhikara <satya@chasteritsolutions.com>
2026-03-18 10:32:32 +0530

    Added User DAL

---
+++ Code/tata_momerial.web.UI/tata_momerial.web.dal/userdal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tata_momerial.web.dal
+{
+    public sealed class UserDAL
+    {
+        internal UserDAL() { }
+
+
+    }
+}
